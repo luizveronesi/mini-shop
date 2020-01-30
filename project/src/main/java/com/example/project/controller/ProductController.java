ac9b280fb371153f3d97725fc39eb56e3f3dbf21
Luiz Veronesi <luiz@texugo.com.br>
2020-01-30 11:04:51 -0300

    último update para upload

--- project/src/main/java/com/example/project/controller/ProductController.java
+++ project/src/main/java/com/example/project/controller/ProductController.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,7 +28,7 @@ public class ProductController {
 		return ResponseEntity.ok(productService.list());
 	}
 
-	@RequestMapping(value = "/upload/{id}", method = RequestMethod.POST, consumes = "multipart/form-data")
+	@PostMapping(value = "/upload/{id}")
 	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file, @PathVariable Integer id) {
 		return new ResponseEntity<>(productService.upload(file, id), HttpStatus.OK);
     }
