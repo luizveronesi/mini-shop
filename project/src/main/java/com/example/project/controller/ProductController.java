3b9df30e3038598672a449d1d8350a09c0e22b72
luiz veronesi <luiz@texugo.com.br>
2020-09-02 14:41:23 -0300

    exemplo de upload (com erro)

--- project/src/main/java/com/example/project/controller/ProductController.java
+++ project/src/main/java/com/example/project/controller/ProductController.java
@@ -8,6 +8,9 @@ import com.example.project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,15 @@ public class ProductController {
 	@GetMapping(value = "/list")
 	public ResponseEntity<List<Product>> findAll() {
 		return ResponseEntity.ok(productService.findAll());
+	}
+
+	@GetMapping(value = "/get/{id}")
+	public ResponseEntity<Product> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(productService.get(id));
+	}
+
+	@PostMapping(value = "/upload")
+	public ResponseEntity<Boolean> upload(@RequestBody xxx) {
+		return ResponseEntity.ok(productService.upload(xxx));
 	}
 }
--- project/src/main/java/com/example/project/service/ProductService.java
+++ project/src/main/java/com/example/project/service/ProductService.java
@@ -1,10 +1,13 @@
 package com.example.project.service;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.project.domain.Product;
 import com.example.project.repository.ProductRepository;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,13 @@ public class ProductService {
 
 	public List<Product> findAll() {
 		return productRepository.findAll();
+	}
+
+	public Product get(Integer id) {
+		return productRepository.getOne(id);
+	}
+
+	public Boolean upload(xxx) {
+		// grvar o arquivo numa pasta local
 	}
 }
