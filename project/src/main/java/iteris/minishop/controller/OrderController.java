321f2f0fc51a0a4dc9d263e69d4b353cac47b8be
Luiz Veronesi <luiz@texugo.com.br>
2020-12-07 13:58:58 -0300

    improvs

--- project/src/main/java/iteris/minishop/controller/OrderController.java
+++ project/src/main/java/iteris/minishop/controller/OrderController.java
@@ -2,14 +2,14 @@ package iteris.minishop.controller;
 
 import java.util.List;
 
-import iteris.minishop.domain.Order;
-import iteris.minishop.service.OrderService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import iteris.minishop.domain.Order;
+import iteris.minishop.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
@@ -17,6 +17,11 @@ public class OrderController {
 
 	@Autowired
 	private OrderService orderService;
+
+	@PostMapping(value = "/page")
+	public ResponseEntity<Page<Order>> list() {
+		return ResponseEntity.ok(orderService.list());
+	}
 
 	@GetMapping(value = "/list")
 	public ResponseEntity<List<Order>> list() {
--- project/src/main/java/iteris/minishop/service/OrderService.java
+++ project/src/main/java/iteris/minishop/service/OrderService.java
@@ -3,9 +3,13 @@ package iteris.minishop.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import iteris.minishop.domain.Order;
+import iteris.minishop.domain.OrderItem;
+import iteris.minishop.repository.OrderItemRepository;
 import iteris.minishop.repository.OrderRepository;
 
 @Service
@@ -14,7 +18,23 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     public List<Order> list() {
         return orderRepository.findAll();
+    }
+
+    public Page<Order> page(Integer start, Integer limit) {
+        PageRequest request = PageRequest.of(start, limit);
+        return orderRepository.findAll(request);
+    }
+
+    public void remove(Integer id) {
+        PageRequest request = PageRequest.of(0, 5);
+        Page<OrderItem> page = orderItemRepository.findAll(request);
+
+        orderItemRepository.removeByOrder(id);
+        orderRepository.deleteById(id);
     }
 }
