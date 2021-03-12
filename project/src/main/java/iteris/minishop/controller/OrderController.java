1ac89008d326e5f8dac51e007bf4f24402e79671
luiz veronesi <luiz@texugo.com.br>
2021-03-12 09:48:30 -0300

    includes page example

--- project/src/main/java/iteris/minishop/controller/OrderController.java
+++ project/src/main/java/iteris/minishop/controller/OrderController.java
@@ -2,14 +2,16 @@ package iteris.minishop.controller;
 
 import java.util.List;
 
-import iteris.minishop.domain.Order;
-import iteris.minishop.service.OrderService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import iteris.minishop.domain.Order;
+import iteris.minishop.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
@@ -21,5 +23,10 @@ public class OrderController {
 	@GetMapping(value = "/list")
 	public ResponseEntity<List<Order>> list() {
 		return ResponseEntity.ok(orderService.list());
+	}
+
+	@GetMapping(value = "/page/{page}/{start}")
+	public ResponseEntity<Page<Order>> page(@PathVariable Integer page, @PathVariable Integer start) {
+		return ResponseEntity.ok(orderService.page(page, start));
 	}
 }
--- project/src/main/java/iteris/minishop/service/OrderService.java
+++ project/src/main/java/iteris/minishop/service/OrderService.java
@@ -1,8 +1,11 @@
 package iteris.minishop.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import iteris.minishop.domain.Order;
@@ -16,5 +19,15 @@ public class OrderService {
 
     public List<Order> list() {
         return orderRepository.findAll();
+    }
+
+    public Order create() {
+        Order order = new Order();
+        order.setOrderDate(new Date());
+        return orderRepository.save(order);
+    }
+
+    public Page<Order> page(Integer page, Integer size) {
+        return orderRepository.findAll(PageRequest.of(page, size));
     }
 }
